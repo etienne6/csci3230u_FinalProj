6629c5762988a5bee2d2c4ec5a96d5cc36fddd70
etienne <etienne.caronan@uoit.net>
2019-03-20 11:57:34 -0400

    fixed bootstrap theme and added navigation through different pages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+//app.use(express.static("file_directory")); directory where we're gonna be serving files from
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // configure sessions
@@ -15,12 +16,33 @@ app.use(session({
     saveUninitialized: false,
     secret: 'something something something'
 }));
-
 //setting up the views and the view engine. Will be using pug templating for this proj
-
-// load view engine, can also use path.join if path is imported
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
+//Configuring the database
+mongoose.Promise = global.Promise //don't know what it does
+mongoose.connect('mongodb://localhost:27017/HoopsHub',
+    { useNewUrlParser: true },
+    function (error) {
+        if (error) {
+            return console.error('Unable to connect:', error);
+        }
+    });
+mongoose.set('useCreateIndex', true); //?? read on this
+
+//schemas for the database
+var schema = mongoose.Schema;
+var userSchema = new schema({
+    username: {
+        type: String,
+        unique: true,
+        index: true
+    },
+    Password: String
+}, { collection: 'users' });
+//what does first argument do again?
+var user = mongoose.model('user', userSchema);
+
 
 //ROUTES
 app.get('/', function (request, response) {
@@ -29,11 +51,24 @@ app.get('/', function (request, response) {
 });
 app.get('/main', function (request, response) {
     username = request.session.username;
-    response.render('main', { title: "HoopsHub Home", username:username});
+    response.render('main', { title: "HoopsHub Home", username: username });
 });
 app.get('/login', function (request, response) {
     response.render('login', { title: "Login" });
 });
+app.get('/register', function (request, response) {
+    response.render('register', { title: "Login" });
+});
+//NAVBAR ROUTES
+app.get('/college', function (request, response) {
+    response.render('college', { title: "College" });
+})
+app.get('/international', function (request, response) {
+    response.render('international', { title: "International" });
+})
+app.get('/nba', function (request, response) {
+    response.render('nba', { title: "NBA" });
+})
 app.post('/processLogin', function (request, response) {
     /*
         how does this work?
@@ -50,13 +85,6 @@ app.post('/processLogin', function (request, response) {
     //IF SUCCESSFUL AUTHENTICATION
     response.render('main', { username: username });
 });
-//NAVBAR ROUTES
-app.get('/college', function(request, response) {
-	response.render('college', {title: "College | Homepage"});
-})
-
-
-
 
 //setting up the port
 app.set('port', process.env.PORT || 3000);
