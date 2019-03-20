e0edb6563e59a8e32552af5a7c38b711963a653b
etienne <etienne.caronan@uoit.net>
2019-03-19 20:10:56 -0400

    added authentication and session tracking

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
 //first things first, create a home page
 var express = require('express');
-var app = express();
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var uuid = require('uuid/v1');
 
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //setting up the port
 app.set('port', process.env.PORT || 3000);
 //setting up the views and the view engine. Will be using pug templating for this proj
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-
-
-
+// configure sessions
+app.use(session({
+    genid: function (request) { return uuid(); },
+    resave: false,
+    saveUninitialized: false,
+    secret: 'something something something'
+}));
 //ROUTES
-app.listen(app.get('port'), function() {
-	console.log('Server listening on port ' + app.get('port'));
+app.get('/', function (request, response) {
+    username = request.session.username;
+    response.render('main', { title: "HoopsHub Home", username: username });
 });
-app.get('/', function(request, response) {
-    response.render('main', {title: "HoopsHub Home"});
-    //request.sendfile('')
+app.get('/main', function (request, response) {
+    username = request.session.username;
+    response.render('main', { title: "HoopsHub Home", username:username});
 });
-app.get('/main', function(request, response) {
-    response.render('main', {title: "HoopsHub Home"});
-    //request.sendfile('')
-});
-app.get('/login', function(request, response) {
-    response.render('login', {title: "Login"});
-});
-//TO do: add a route for user login
 
+app.get('/login', function (request, response) {
+    response.render('login', { title: "Login" });
+});
+app.post('/processLogin', function (request, response) {
+    /*
+        how does this work?
+        - how does body parser know to get user name and password
+        - what does method: post mean under login
+        why does process Login go to server not found? Is it because we need a response?
+        response.render() -- see documentation
+    */
+    var username = request.body.username;
+    var password = request.body.password;
 
+    request.session.username = username
+    //TO DO: authentication - check if the user exists. 
+    //IF SUCCESSFUL AUTHENTICATION
+    response.render('main', { username: username });
+});
 
+//web listener
+app.listen(app.get('port'), function () {
+    console.log('Server listening on port ' + app.get('port'));
+});
