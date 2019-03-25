0db00a2b87238e81346706306c4a2086c110ddbc
etienne <etienne.caronan@uoit.net>
2019-03-24 23:30:53 -0400

    fixed errors + added contents to NBA page

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-//AUXILLARY FUNCTIONS 
 function getDate() {
     var d = new Date();
     var curr_date = d.getDate();
@@ -26,7 +25,6 @@ function renderText(text) {
     text = text.split("\n");
     return text;
 }
-//SETTING UP MIDDLEWARE
 app.use(express.static("public")); //directory where we're gonna be serving files from
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -37,13 +35,12 @@ app.use(session({
     saveUninitialized: false,
     secret: 'something something something'
 }));
-
-//SETTING UP VIEWS AND VIEW ENGINE
+//setting up the views and the view engine. 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 //Configuring the database
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise //don't know what it does
 mongoose.connect('mongodb://localhost:27017/HoopsHub',
     { useNewUrlParser: true },
     function (error) {
@@ -51,8 +48,7 @@ mongoose.connect('mongodb://localhost:27017/HoopsHub',
             return console.error('Unable to connect:', error);
         }
     });
-mongoose.set('useCreateIndex', true);
-
+mongoose.set('useCreateIndex', true); //?? read on this
 //schemas for the database
 var schema = mongoose.Schema;
 var userSchema = new schema({
@@ -70,78 +66,26 @@ var contentSchema = new schema({
     author: String,
     content: String,
     tags: Array
-}, { collection: 'contents' });
-
-//Defining model for the database
+}, { collection: 'sample_contents' });
+//Defining model for db
 var user = mongoose.model('user', userSchema);
-var contents = mongoose.model('content', contentSchema);
-
-//ROUTING
+var sample_content = mongoose.model('content', contentSchema);
+//ROUTES
 app.get('/', function (req, res) {
     res.redirect('/main')
 });
-app.get('/register', function (req, res) {
-    res.render('register', { title: "Login" });
-});
-//NAVBAR ROUTES
-app.get('/college', function (req, res) {
-    res.render('college', { title: "College", username: req.session.username });
-})
-app.get('/international', function (req, res) {
-    res.render('international', { title: "International", username: req.session.username });
-})
-app.get('/nba', function (req, res) {
-    res.render('nba', { title: "NBA", username: req.session.username });
-})
-app.get('/addcontent', function (req, res) {
-    if (!req.session.username) {
-        res.redirect("/login")
-    } else {
-        res.render('addContent', { title: "Submit to HoopsHub", username: req.session.username });
-    }
-})
-//Main routing
-app.post('/main', function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
 
-    user.findOne({ username: username }, function (error, user) {
-        //needs to have a code that deals with errors
-        if (error) {
-            return handleError(error);
-        } else {
-            if (user.password != password) {
-                console.log("password error!");
-                res.render("login");
-            } else {
-                req.session.username = username
-                contents.find({}, function (error, results) {
-                    if (error) return next(error);
-                    res.render("main", { title: "HoopsHub Main", username: username, contents: results })
-                })
-            }
-        }
-    });
+app.get('/login', function (req, res) {
+    res.render('login', { title: "Login" });
 });
-app.get('/main', function (req, res) {
-    username = req.session.username;
-    //CODE CAUSING ERROR
-    contents.find({}, function (error, results) {
-        if (error) return next(error);
-        res.status(200).render("main", { title: "HoopsHub Main", username: username, contents: results })
-    })
-});
-//Login routes
-//processes registration
 app.post('/login', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
     var fullname = req.body.fullName
-
     var newUser = new user({ username: username, password: password, fullname: fullname});
-    //how does the stack work for this?
     newUser.save(function (error) {
         if (error) {
+            //TODO: code to deal with error
             //error is thrown because username has property: unique: true
             res.render("register");
             console.log("username already exists");
@@ -150,30 +94,75 @@ app.post('/login', function (req, res) {
         }
     });
 });
-app.get('/login', function (req, res) {
-    res.render('login', { title: "Login" });
+app.get('/register', function (req, res) {
+    res.render('register', { title: "Login" });
+});
+//NAVBAR ROUTES
+app.get('/college', function (req, res) {
+    //todo:query sample_contents only for articles with college tags
+    res.render('college', { title: "COLLEGE", username: req.session.username });
+})
+app.get('/international', function (req, res) {
+    //todo:query sample_contents only for articles with international tags
+    res.render('international', { title: "International", username: req.session.username });
+})
+app.get('/nba', function (req, res) {
+    sample_content.find({tags: "NBA"}, function(err, results){
+        res.render('nba', {title: "NBA | HoopsHub", contents:results, username: req.session.username});
+    });
+})
+app.get('/addcontent', function (req, res) {
+    if (!req.session.username) {
+        res.redirect("/login")
+    } else {
+        res.render('addContent', { title: "Submit to HoopsHub", username: req.session.username });
+    }
+})
+//MAIN PAGE 
+app.get('/main', function (req, res) {
+    username = req.session.username;
+    //TODO: get the objects from contents collection
+    sample_content.find({}, null, {sort:{title:-1}}, function (error, results) {
+        if (error) return next(error);
+        res.render("main", { title: "HoopsHub Main", username: username, contents: results })
+    })
+});
+app.post('/main', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    user.findOne({ username: username }, function (error, user) {
+        //TODO: Handle error when database is empty
+        if (error) {
+            return handleError(error);
+        } else {
+            if (user.password != password) {
+                console.log("password error!");
+                res.render("login");
+            } else {
+                req.session.username = username
+                sample_content.find({}, function (error, results) {
+                    if (error) return next(error);
+                    res.render("main", { title: "HoopsHub Main", username: username, contents: results })
+                })
+            }
+        }
+    });
 });
 //add Article contents to database to the database. Routing for post contents
 app.post("/submitContent", function (req, res) {
-    var username = req.session.username //used for querying author name
     var title = req.body.title;
     var tags = renderTags(req.body.tags.split(","));
     var content = req.body.content;
-    var author;
-    //TODO: change author to an obj query by username
-    user.findOne({username:username},function(err, user){
-        //TODO: catch block for error
-        author = user.fullname;
-    });
+    var author = req.session.username;
     var date = getDate();
-    var newContent = new contents({
+    var newContent = new sample_content({
         title: title,
         date: date,
         author: author,
         content: content,
         tags: tags
     });
-    //TODO: add prompt that users cannot submit an empty field
     newContent.save(function (error) {
         if (error) {
             //error is thrown because username has property: unique: true
@@ -187,10 +176,9 @@ app.post("/submitContent", function (req, res) {
 //renders individual posts --- see main.pug
 app.get('/contents/:mainTag/:id/:article', function (req, res, next) {
     var ID = req.params.id;
-    contents.findById(ID, function (err, article) {
+    sample_content.findById(ID, function (err, article) {
         contents = renderText(article.content);
-        console.log(article.author);
-        res.render("article", { article: article, contents: contents, username: req.session.username });
+        res.render("article", { article: article, contents: contents });
     });
 });
 //setting up the port
